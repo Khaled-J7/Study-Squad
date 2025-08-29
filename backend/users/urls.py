3097d09ec28f35792414d76c102c8b9ffbfa0d81
Rayen-JN <rayenchuk@gmail.com>
2025-08-29 17:19:45 +0100

    urls.py update

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,11 @@
 # backend/users/urls.py
 from django.urls import path
-from .views import explore_view
+from .views import explore_view, logout_view, register_view, current_user_view
 
 urlpatterns = [
     # This one URL will now handle all our explore page requests
     path("explore/", explore_view, name="explore"),
+    path("auth/register/", register_view, name="register"),
+    path("auth/logout/", logout_view, name="logout"),
+    path("auth/user/", current_user_view, name="current-user"),
 ]
