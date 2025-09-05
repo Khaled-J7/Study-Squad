9aa2b743a7579841eb2a0f5b05a8dd9bebf57b02
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2025-09-05 09:27:32 +0100

    Spinner Componenet Implementation

---
+++ frontend/src/components/common/Spinner.jsx
@@ -0,0 +1,12 @@
+// frontend/src/components/common/Spinner.jsx
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="spinner-overlay">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
