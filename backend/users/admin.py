46fed0323759a4cd3016d69e675f459034e9c4f5
Khaled-J7 <jallouli.khaled.2001@gmail.com>
2025-09-18 18:23:57 +0100

    uncomment studio admin site to access it from Django Admin Panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,7 +6,7 @@ from .models import Profile, Tag, Studio, Lesson, Post, Comment
 # Register the models here so they appear in the admin site
 admin.site.register(Profile)
 admin.site.register(Tag)
-# admin.site.register(Studio)
+admin.site.register(Studio)
 admin.site.register(Lesson)
 admin.site.register(Post)
 admin.site.register(Comment)
